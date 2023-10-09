8128cd42926da378b50630096c9b3737e656d3e9
jeff <devil1032@gmail.com>
2023-10-09 19:50:58 +0900

    add: table script (draft)

---
+++ console.sql
@@ -0,0 +1,33 @@
+create schema mydb;
+
+create table user
+(
+    id         bigint unsigned primary key auto_increment,
+    name       varchar(50)  not null comment '유저의 실제이름',
+    nick       varchar(20)  not null comment '유저의 닉네임',
+    email      varchar(100) not null comment '유저 이메일. 반드시 있어야 한다.',
+    phone_num  varchar(13)  null comment '핸드폰이 없어도 가입 할 수 있다.',
+    created_dt datetime     not null default current_timestamp(),
+    updated_dt datetime     not null default current_timestamp()
+);
+
+create table board
+(
+    id         bigint unsigned primary key auto_increment,
+    user_id    bigint unsigned not null comment 'user 테이블 id 컬럼',
+    title      varchar(255)    not null comment '게시글 제목',
+    created_dt datetime        not null default current_timestamp(),
+    updated_dt datetime        not null default current_timestamp()
+);
+
+
+create table board_content
+(
+    id bigint unsigned primary key auto_increment,
+    board_id bigint unsigned not null comment 'board 테이블 id 컬럼',
+    content longtext not null comment '게시글 상세 내용',
+    created_dt datetime not null default current_timestamp(),
+    updated_dt datetime not null default current_timestamp()
+
+)
+
